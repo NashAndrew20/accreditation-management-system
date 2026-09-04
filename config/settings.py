ed95d23c190d0f0acfa287a8bdad6a1698d9ede6
charles <charles.ponto@jmc.edu.ph>
2026-09-04 16:53:10 +0800

    modernize login page design

--- config/settings.py
+++ config/settings.py
@@ -172,11 +172,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Django's built-in Redis cache backend. Django running directly on the host
